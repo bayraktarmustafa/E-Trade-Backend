cb60ac7b05e0c7b512295fa95052324ec878436d
BAYRAKTAR <m.bayraktar01@hotmail.com>
2022-12-15 13:25:33 +0300

    update

---
+++ Business/Constants/Messages.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Constants
+{
+    public static class Messages
+    {
+        public static string ProductAdded = "URUN EKLENDI";
+        public static string ProductNameInvalid = "URUN ISMI GECERSIZ";
+        public static string MaintenanceTime = "SISTEM BAKIMDA";
+        public static string ProductsListed = " URUNLER LISTELENDI";
+        public static string ProductDeleted = "URUN SILINDI";
+
+    }
+}
---
+++ Entities/Concrete/Product.cs
@@ -0,0 +1,20 @@
+﻿
+using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Concrete
+{
+    public class Product:IEntity //Default is Internal Class the class let me access only from this project, I want to access it from other projects so i defined it as "public". 
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public short UnitsInStock { get; set; }
+        public decimal UnitPrice { get; set; }
+
+
+
+    }
+}
